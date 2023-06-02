0744e8deef400e0d1c526b9b97e6c1c7a2d17063
Hithel <Edward22cd@gmail.com>
2023-06-01 21:28:09 -0500

    Primmer Commit

---
+++ js/mostrarPersona.js
@@ -0,0 +1,22 @@
+export default function mostrarPersona(data){
+    let tbodyPersonas = document.querySelector("#tbodyPersonas");
+
+    tbodyPersonas.innerHTML = "";
+
+    data.forEach((persona)=>{
+        let tr = document.createElement("tr");
+        tr.setAttribute("id",`${persona.id}`);
+        tr.setAttribute("class","tr");
+        tr.innerHTML = `
+        <td>${persona.nombre}</td>
+        <td>${persona.edad}</td>
+        <td>${persona.sexo}</td>
+        <td>
+            <input type="submit" data-accion="Eliminar" value="Eliminar" class="btn-guardar bg-danger border-0 rounded bg-secondary px-2">
+            <input type="submit" data-accion="Actualizar" value="Actualizar" class="btn-guardar bg-warning border-0 rounded bg-secondary px-2">
+        </td>
+        `;
+
+        tbodyPersonas.appendChild(tr);
+    });
+}
